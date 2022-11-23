b0ce4ae5769546bb0b91db93fcb913ebd7389f9a
Noah Buchanan <noah6544@gmail.com>
2022-11-22 22:55:11 -0500

    AFTER PROBABLY 60 HOURS FROM IN CLASS AT HOME AND SITTING IN FRONT OF MY PC LAST 2 DAYS ON MY BREAK
    ITS DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! until i find another broken thing

--- THEGAPWEBSITE/settings.py
+++ THEGAPWEBSITE/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g$(92h!20tza#gqa5+gi1lpkr-k*rp^_r!e%k5@z$_@9bc3813'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://noah6544.pythonanywhere.com/','http://pythonanywhere.com/']
 
 
 # Application definition
