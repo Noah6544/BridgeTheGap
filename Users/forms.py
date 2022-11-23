6a77b6f83ce0eddfec262ee360ec3010ab7f1ee0
Noah Buchanan <noah6544@gmail.com>
2022-11-22 22:25:45 -0500

    Added a lot of animations. Made the Quiz look cleaner. Added a coding section to the section model and made that only appear if the field exists. HELP the course menu animation isn't working. i'm uploading this so u can rollback to later. it sucks so bad tho sorry it was better before.

--- Users/forms.py
+++ Users/forms.py
@@ -4,9 +4,27 @@ from django.forms import ModelForm
 from django.contrib.auth.models import User
 
 class CreateUserForm(UserCreationForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+
     class Meta:
         model = User
         fields = [
             'username', 'first_name','last_name', 'email','password1','password2'
         ]
+
+        widgets = {
+            'username':forms.TextInput(attrs={'class':'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+
+
+        }
+
+    def __int__(self, *args,**kwargs):
+        super(CreateUserForm,self).__init__(*args,**kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['id_password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
 
--- course/templatetags/course_in_category.py
+++ course/templatetags/course_in_category.py
@@ -7,3 +7,12 @@ from course.models import Chapter, Topic
 def in_category(ch,topicins): #LEAVE AS ch. cuz then it's basicallly a blank argument and just uses the 2nd topic and not the first. please. this was a dumb bug that took me a minute to resolve
     #i resolved it 100% by accident by not using the first argument
     return Chapter.objects.filter(topic=topicins)
+
+@register.filter #LETS GO FIRST TRY I'M SO GOOD AT PYTHON LETS GOOOOO
+def user_tag(content,user):
+    return content.replace('{{ user.username }}',user.username)
+
+
+@register.filter
+def customlinebreaks(content,text):
+    return content.replace(text,"<br>")
